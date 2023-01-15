2bd49d7a9052dc54018d2f09d601158e966093df
Remy Benza <47558082+rbenza@users.noreply.github.com>
2023-01-15 15:48:14 +0100

    Start with public holidays 3

--- library/src/main/java/com/alexstyl/contactstore/utils/FlowExtensions.kt
+++ library/src/main/java/com/alexstyl/contactstore/utils/FlowExtensions.kt
@@ -1,8 +1,11 @@
 package com.alexstyl.contactstore.utils
 
+import android.app.Fragment
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.onStart
 
 internal fun Flow<Unit>.startImmediately(): Flow<Unit> {
     return this.onStart { emit(Unit) }
 }
+
+
